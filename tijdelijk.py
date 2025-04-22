ddcf254d1fdef49323f8ca7c7cf6a6cb8b625e69
Mike Helleman <mhelleman@tudelft.nl>
2025-04-22 16:13:14 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,23 @@
+prijzen = {
+    "aardbei": 3,
+    "vanille": 4,
+    "chocolade": 5
+}
+
+aanbieding = prijzen["aardbei"] * 0.8
+
+reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter – slechts € {aanbieding}"
+
+index = reclame_tekst.find("0", reclame_tekst.find(",") + 1)
+
+reclame_tekst2 = reclame_tekst[:index]
+
+reclame_tekst3 = reclame_tekst2.upper()
+
+reclame_tekst4 = reclame_tekst3.split()
+
+for el in reclame_tekst4:
+    if len(el) >= 5:
+        print(el.upper())  # Woord in hoofdletters
+    else:
+        print(el.lower())
